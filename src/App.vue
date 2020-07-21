dfbae11d96b27a66587cf2ebe05ec9cd0fb4ece4
Uayeb85 <gustavo.aa18@gmail.com>
2020-07-21 18:50:21 -0500

    disney tailwind vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+  <template>
+    <div id="app" class="flex bg-gray-900 text-white min-h-screen" >
+        <sidebar-navigation />
+      <div class="w-full">
+        <div class="container mx-auto ">
+          <router-view/>
+
+        </div>
+
+      </div>
+  </div>
+</template>
+
+
+<script>
+import SidebarNavigation from '@/components/SidebarNavigation'
+
+
+export default {
+  components:{
+    SidebarNavigation
+  }
+
+}
+</script>
+
+<style src="./css/main.css"> </style>
